19203c437e2b527e19382e62f395e9319d0eac77
blessedr038-web <blessedr038@gmail.com>
2026-02-10 19:05:13 +0300

    Initial Committed

---
+++ datatypes.py
@@ -0,0 +1,30 @@
+
+age = 18  #Integer
+temperature = 34.89  #Float
+greeting = "Hi!" #String
+isMarried = True #Boolean
+
+print("I am",age,"years old")
+print("The current body temperature of the patient is",temperature, "degrees Celsius")
+print(greeting, "Blessed")
+print("Are you married? :",isMarried)
+
+
+# Data Structures - Multiple values stored in a single variable
+cars = ["Toyota", "Mercedes", "Audi", "Volvo", "BMW"] #List - Ordered and changeable
+fruits = ("apple", "banana", "cherry", "mango") #Tuple -Ordered and unchangeable
+countries = {"Italy", "Germany","France", "Spain"} #Set - Unchangeable
+student = {
+    "firstname" : "John",
+    "age" : 19,
+    "course" :"FullStack",
+    "gender" : "Male"
+}
+
+print(cars)
+print(fruits)
+print(countries)
+print(student)
+print(student.get("firstname"))
+
+#Typecasting
---
+++ variables.py
@@ -0,0 +1,15 @@
+#Variable is a container used to store a value
+
+firstname = "Blessed"
+school = "emobilis"
+lastname = "Cheptoo"
+num1 = 56
+num2 = 20
+language = "Python"
+
+print(firstname)
+print(school)
+print(firstname,lastname)
+print(school,"is a coding school")
+print(num1 + num2)
+print(language, "is a web programming language")
